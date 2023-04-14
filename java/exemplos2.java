272e91038118aa80be840d6d8416959b296d7356
BELIVABLE-Pedro <125685178+BELIVABLE-Pedro@users.noreply.github.com>
2023-04-14 11:32:12 -0300

    m

--- java/exemplos2.java
+++ java/exemplos2.java
@@ -1,3 +1,17 @@
-public class exemplos2 {
+/*Constante ->Variável em Java em que não posso alterar o  valor 
+ * Usamos a palavra chave FINAL
+ */
+
+
+ public class exemplos2 {
+   
+    public static final int TAXA = 10; 
+ public static final double PI= 3.14;
+// public -> A constante pode ser acessada em qualquer parte do código
+//static -> Essa constante é compartilhada entre todas as classes do meu programa
+// final -> Não posso alterar o valor
+public static void main(String[] args) {
     
+} 
+
 }
